bb946fc5b8e0ac9103ac6807639656eb8c477a86
Bashir Manafikhi <bashir.manafikhi@gmail.com>
2024-10-04 08:14:35 +0300

    add more projects

---
+++ MachineLearningSamples/ProductSalesAnomalyDetection/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+using System.Collections.Generic;
+
+namespace ProductSalesAnomalyDetection
+{
+    class Program
+    {
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "product-sales.csv");
+        //assign the Number of records in dataset file to constant variable
+        const int _docsize = 36;
+        static void Main(string[] args)
+        {
+            // Create MLContext to be shared across the model creation workflow objects
+            MLContext mlContext = new MLContext();
+
+            //STEP 1: Common data loading configuration
+            IDataView dataView = mlContext.Data.LoadFromTextFile<ProductSalesData>(path: _dataPath, hasHeader: true, separatorChar: ',');
+
+            // Spike detects pattern temporary changes
+            DetectSpike(mlContext, _docsize, dataView);
+
+            // Changepoint detects pattern persistent changes
+            DetectChangepoint(mlContext, _docsize, dataView);
+        }
+
+        static void DetectSpike(MLContext mlContext, int docSize, IDataView productSales)
+        {
+            Console.WriteLine("Detect temporary changes in pattern");
+
+            // STEP 2: Set the training algorithm
+            var iidSpikeEstimator = mlContext.Transforms.DetectIidSpike(outputColumnName: nameof(ProductSalesPrediction.Prediction), inputColumnName: nameof(ProductSalesData.numSales), confidence: 95, pvalueHistoryLength: docSize / 4);
+
+            // STEP 3: Create the transform
+            // Create the spike detection transform
+            Console.WriteLine("=============== Training the model ===============");
+            ITransformer iidSpikeTransform = iidSpikeEstimator.Fit(CreateEmptyDataView(mlContext));
+
+            Console.WriteLine("=============== End of training process ===============");
+            //Apply data transformation to create predictions.
+            IDataView transformedData = iidSpikeTransform.Transform(productSales);
+
+            var predictions = mlContext.Data.CreateEnumerable<ProductSalesPrediction>(transformedData, reuseRowObject: false);
+
+            Console.WriteLine("Alert\tScore\tP-Value");
+
+            foreach (var p in predictions)
+            {
+                var results = $"{p.Prediction[0]}\t{p.Prediction[1]:f2}\t{p.Prediction[2]:F2}";
+
+                if (p.Prediction[0] == 1)
+                {
+                    results += " <-- Spike detected";
+                }
+
+                Console.WriteLine(results);
+            }
+            Console.WriteLine("");
+        }
+
+        static void DetectChangepoint(MLContext mlContext, int docSize, IDataView productSales)
+        {
+            Console.WriteLine("Detect Persistent changes in pattern");
+
+            //STEP 2: Set the training algorithm
+            var iidChangePointEstimator = mlContext.Transforms.DetectIidChangePoint(outputColumnName: nameof(ProductSalesPrediction.Prediction), inputColumnName: nameof(ProductSalesData.numSales), confidence: 95, changeHistoryLength: docSize / 4);
+
+            //STEP 3: Create the transform
+            Console.WriteLine("=============== Training the model Using Change Point Detection Algorithm===============");
+            var iidChangePointTransform = iidChangePointEstimator.Fit(CreateEmptyDataView(mlContext));
+            Console.WriteLine("=============== End of training process ===============");
+
+            //Apply data transformation to create predictions.
+            IDataView transformedData = iidChangePointTransform.Transform(productSales);
+
+            var predictions = mlContext.Data.CreateEnumerable<ProductSalesPrediction>(transformedData, reuseRowObject: false);
+
+            Console.WriteLine("Alert\tScore\tP-Value\tMartingale value");
+
+            foreach (var p in predictions)
+            {
+                var results = $"{p.Prediction[0]}\t{p.Prediction[1]:f2}\t{p.Prediction[2]:F2}\t{p.Prediction[3]:F2}";
+
+                if (p.Prediction[0] == 1)
+                {
+                    results += " <-- alert is on, predicted changepoint";
+                }
+                Console.WriteLine(results);
+            }
+            Console.WriteLine("");
+        }
+
+        static IDataView CreateEmptyDataView(MLContext mlContext)
+        {
+            // Create empty DataView. We just need the schema to call Fit() for the time series transforms
+            IEnumerable<ProductSalesData> enumerableData = new List<ProductSalesData>();
+            return mlContext.Data.LoadFromEnumerable(enumerableData);
+        }
+    }
+}
